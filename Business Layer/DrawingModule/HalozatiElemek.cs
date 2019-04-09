776a6472899d60b1d66543af3c17ed3a0f28089a
janoserdody <erdody.janos@gmail.com>
2019-04-09 23:17:17 +0200

    ErrorMessage EventHandler ready

---
+++ Business Layer/DrawingModule/HalozatiElemek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.DrawingModule
+{
+    public class HalozatiElemek
+    {
+        public int ID;
+        public String nev;
+
+        public HalozatiElemek(int p1, string p2)
+        {
+            ID = p1;
+            nev = p2;
+
+        }
+    }
+}
---
+++ Business Layer/DrawingModule/Vonal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.DrawingModule
+{
+    public class Vonal
+    {
+        public int ID;
+        public String nev;
+        public int Obj1;
+        public int Obj2;
+        public Vonal(int p1, string p2, int p3, int p4)
+        {
+            ID = p1;
+            nev = p2;
+            Obj1 = p3;
+            Obj2 = p4;
+        }
+
+    }
+}
